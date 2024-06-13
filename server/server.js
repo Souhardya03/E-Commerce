e0feef8165d8861b32cef5d11a64c7c80e8d76bd
darkwarrior2003 <souhardyadeb2016@gmail.com>
2024-06-13 21:09:30 +0530

    update

--- server/server.js
+++ server/server.js
@@ -1,36 +1,31 @@
 require("dotenv").config();
-const express = require("express");
+const express = require("express")
 const cors = require("cors");
 const app = express();
-const authRoute = require("./routes/authRoute.js");
-const categoryRoute = require("./routes/categoryRoute.js");
-const productRoute = require("./routes/productRoute.js");
-const imageRoute = require("./routes/imageRouter.js");
-const connectDB = require("./config/db.js");
-
+const authRoute = require("./routes/authRoute.js")
+const categoryRoute = require("./routes/categoryRoute.js")
+const productRoute = require("./routes/productRoute.js")
+const imageRoute = require("./routes/imageRouter.js")
+const connectDB = require("./config/db.js")
 connectDB();
-
-const corsOptions = {
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true, 
-};
-
-app.use(cors(corsOptions));
+const corsoption = {
+    origin : "*",
+    methods : "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials : true, 
+}
+app.use(cors(corsoption))
 app.use(express.json());
 
-// Routes
+//Routes
 app.use("/api/auth", authRoute);
-app.use("/api/categories", categoryRoute);
-app.use("/api/products", productRoute);
-app.use("/api/images", imageRoute);
-app.use("/images", express.static('uploads/images'));
+app.use("/api/auth", categoryRoute);
+app.use("/api/auth", productRoute);
+app.use("/api/auth", imageRoute);
+app.use("/images",express.static('uploads/images'));
 
-app.get("/", (req, res) => {
-    res.send({ message: "Welcome To E-Commerce Backend" });
-});
-
+app.get("/",(req,res)=>{
+    res.send({message:"Welcome To E-Commerce Backend"});
+})
 const port = process.env.PORT || 8000;
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
+console.log(`Server is running at http://localhost:${port}`);
+app.listen(port);
