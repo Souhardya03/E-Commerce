84affb378a57b6b4fe7589b0f588cb970103d8a1
darkwarrior2003 <souhardyadeb2016@gmail.com>
2024-06-16 09:21:13 +0530

    updated

--- server/routes/imageRouter.js
+++ server/routes/imageRouter.js
@@ -1,18 +1,21 @@
 const multer = require("multer");
 const path = require("path");
-const express = require("express")
+const express = require("express");
 const router = express.Router();
-const authMiddlewares =require( "../middlewares/authmiddlewares.js");
-const imageController = require("../controllers/imageController.js")
+const authMiddlewares = require("../middlewares/authmiddlewares.js");
+const imageController = require("../controllers/imageController.js");
+
 const storage = multer.diskStorage({
-  destination: "./uploads/images",
-  filename: (req, file, cb) => {
-    return cb(
-      null,
-      `${file.fieldname}_${file.originalname}`
-    );
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, '../uploads/images'));
   },
+  filename: (req, file, cb) => {
+    cb(null, `${file.fieldname}_${file.originalname}`);
+  }
 });
-const upload = multer({storage:storage})
-router.post("/upload",authMiddlewares.requireSignIn,authMiddlewares.isAdmin,upload.array('product',4),imageController.uploadImage)
-module.exports=router
+
+const upload = multer({ storage });
+
+router.post("/upload", authMiddlewares.requireSignIn, authMiddlewares.isAdmin, upload.array('product', 4), imageController.uploadImage);
+
+module.exports = router;
